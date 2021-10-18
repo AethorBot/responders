5fef0dea0c2f66eabadf2c629975d04481dd1821
Tricked <72335827+SkyBlockDev@users.noreply.github.com>
2021-10-18 22:58:09 +0200

    Add responders

---
+++ README.md
@@ -0,0 +1,5 @@
+## Aethor auto responder repo
+
+This repository hosts all responders for discord servers mapped by server id's.
+
+if you notice any typo's or feel the need to create additional auto responders make a pull request.
---
+++ api/mod.ts
@@ -0,0 +1,57 @@
+import { listenAndServe } from 'https://deno.land/std@0.111.0/http/server.ts';
+import { parse } from 'https://deno.land/std@0.112.0/encoding/toml.ts';
+async function handleRequest(request: Request) {
+	const { pathname, search } = new URL(request.url);
+	const params = new URLSearchParams(search);
+
+	if (pathname.startsWith('/responders')) {
+		const server = params.get('server');
+		if (server) {
+			try {
+				const data = await Deno.readTextFile(
+					`${Deno.cwd()}/data/${server}.toml`
+				);
+				const toml = parse(data);
+				return new Response(JSON.stringify(toml.responder), {
+					headers: {
+						'content-type': 'application/json',
+					},
+				});
+			} catch (e) {
+				return new Response(JSON.stringify({ success: false, message: e }), {
+					headers: {
+						'content-type': 'application/json',
+					},
+				});
+			}
+		}
+	} else if (pathname.startsWith('/all')) {
+		const files = await Deno.readDir(`${Deno.cwd()}/data`);
+
+		const data: Record<string, any> = {};
+		for await (const file of files) {
+			try {
+				const content = await Deno.readTextFile(
+					`${Deno.cwd()}/data/${file.name}`
+				);
+
+				data[file.name.split('.')[0] as string] = parse(content).responder;
+			} catch (e) {
+				console.log(e);
+			}
+		}
+		return new Response(JSON.stringify(data), {
+			headers: {
+				'content-type': 'application/json',
+			},
+		});
+	}
+	return new Response(JSON.stringify({}, null, 2), {
+		headers: {
+			'content-type': 'application/json',
+		},
+	});
+}
+
+console.log('Listening on http://localhost:8080');
+await listenAndServe(':8080', handleRequest);
