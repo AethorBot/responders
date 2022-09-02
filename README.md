c23bf152f1d4153bceaf4bc67fae3b8093d7a594
Tricked <tricked@tricked.pro>
2022-09-02 16:33:11 +0200

    Fresh up the repo

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
-## Aethor auto responder repo
+# Aethor auto responder repo
 
 This repository hosts all responders for discord servers mapped by server id's.
 
-if you notice any typo's or feel the need to create additional auto responders make a pull request.
+if you notice any typo's or feel the need to create additional auto responders
+make a pull request.
+
+## Contributing
+
+Want to add responders for a certain server you are staff in? just fork the repo
+and and create a file in `data/` with the responders you can look at other
+responders for examples. I would also like proof of you being staff in the
+server your're adding responders for.
--- api/mod.ts
+++ api/mod.ts
@@ -1,57 +1,57 @@
-import { listenAndServe } from 'https://deno.land/std@0.111.0/http/server.ts';
-import { parse } from 'https://deno.land/std@0.112.0/encoding/toml.ts';
+import { listenAndServe } from "https://deno.land/std@0.151.0/http/server.ts";
+import { parse } from "https://deno.land/std@0.151.0/encoding/toml.ts";
 async function handleRequest(request: Request) {
-	const { pathname, search } = new URL(request.url);
-	const params = new URLSearchParams(search);
+  const { pathname, search } = new URL(request.url);
+  const params = new URLSearchParams(search);
 
-	if (pathname.startsWith('/responders')) {
-		const server = params.get('server');
-		if (server) {
-			try {
-				const data = await Deno.readTextFile(
-					`${Deno.cwd()}/data/${server}.toml`
-				);
-				const toml = parse(data);
-				return new Response(JSON.stringify(toml.responder), {
-					headers: {
-						'content-type': 'application/json',
-					},
-				});
-			} catch (e) {
-				return new Response(JSON.stringify({ success: false, message: e }), {
-					headers: {
-						'content-type': 'application/json',
-					},
-				});
-			}
-		}
-	} else if (pathname.startsWith('/all')) {
-		const files = Deno.readDir(`${Deno.cwd()}/data`);
+  if (pathname.startsWith("/responders")) {
+    const server = params.get("server");
+    if (server) {
+      try {
+        const data = await Deno.readTextFile(
+          `${Deno.cwd()}/data/${server}.toml`,
+        );
+        const toml = parse(data);
+        return new Response(JSON.stringify(toml.responder), {
+          headers: {
+            "content-type": "application/json",
+          },
+        });
+      } catch (e) {
+        return new Response(JSON.stringify({ success: false, message: e }), {
+          headers: {
+            "content-type": "application/json",
+          },
+        });
+      }
+    }
+  } else if (pathname.startsWith("/all")) {
+    const files = Deno.readDir(`${Deno.cwd()}/data`);
 
-		const data: Record<string, unknown> = {};
-		for await (const file of files) {
-			try {
-				const content = await Deno.readTextFile(
-					`${Deno.cwd()}/data/${file.name}`
-				);
+    const data: Record<string, unknown> = {};
+    for await (const file of files) {
+      try {
+        const content = await Deno.readTextFile(
+          `${Deno.cwd()}/data/${file.name}`,
+        );
 
-				data[file.name.split('.')[0] as string] = parse(content).responder;
-			} catch (e) {
-				console.log(e);
-			}
-		}
-		return new Response(JSON.stringify(data), {
-			headers: {
-				'content-type': 'application/json',
-			},
-		});
-	}
-	return new Response(JSON.stringify({}, null, 2), {
-		headers: {
-			'content-type': 'application/json',
-		},
-	});
+        data[file.name.split(".")[0] as string] = parse(content).responder;
+      } catch (e) {
+        console.log(e);
+      }
+    }
+    return new Response(JSON.stringify(data), {
+      headers: {
+        "content-type": "application/json",
+      },
+    });
+  }
+  return new Response(JSON.stringify({}, null, 2), {
+    headers: {
+      "content-type": "application/json",
+    },
+  });
 }
 
-console.log('Listening on http://localhost:8080');
-await listenAndServe(':8080', handleRequest);
+console.log("Listening on http://localhost:8080");
+await listenAndServe(":8080", handleRequest);
